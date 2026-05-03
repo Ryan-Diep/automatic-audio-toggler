6d5387b168a9e811cf3cabd507b1c235e87a5646
Ryan Diep <ryandiep5@gmail.com>
2026-05-03 03:44:09 -0400

    Revert "add to fix not detecting on startup again"
    This reverts commit 821c812035ac611409d799b7b586bc75c6aeda93.

--- automatic-audio-toggler-go/main.go
+++ automatic-audio-toggler-go/main.go
@@ -152,6 +152,48 @@ func comInit() error {
 	return nil
 }
 
+func correctStartupState(cfg Config) error {
+	if err := comInit(); err != nil {
+		return err
+	}
+	defer ole.CoUninitialize()
+
+	var enumerator *wca.IMMDeviceEnumerator
+	if err := wca.CoCreateInstance(
+		wca.CLSID_MMDeviceEnumerator,
+		0,
+		wca.CLSCTX_ALL,
+		wca.IID_IMMDeviceEnumerator,
+		&enumerator,
+	); err != nil {
+		return err
+	}
+	defer enumerator.Release()
+
+	var device *wca.IMMDevice
+	if err := enumerator.GetDefaultAudioEndpoint(wca.ERender, wca.EConsole, &device); err != nil {
+		return err
+	}
+	defer device.Release()
+
+	var ps *wca.IPropertyStore
+	if err := device.OpenPropertyStore(wca.STGM_READ, &ps); err != nil {
+		return err
+	}
+	defer ps.Release()
+
+	var pv wca.PROPVARIANT
+	if err := ps.GetValue(&wca.PKEY_Device_FriendlyName, &pv); err != nil {
+		return err
+	}
+
+	if strings.Contains(pv.String(), cfg.HeadsetName) {
+		return setDefaultAudioDevice(cfg.SpeakerName)
+	}
+
+	return nil
+}
+
 func setDefaultAudioDevice(deviceName string) error {
 	if err := comInit(); err != nil {
 		return err
@@ -243,7 +285,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	setDefaultAudioDevice(cfg.SpeakerName)
+	correctStartupState(cfg)
 
 	path, err := findDevicePath(uint16(cfg.VendorID), uint16(cfg.ProductID))
 	if err != nil {
@@ -269,12 +311,7 @@ func main() {
 	}()
 
 	buf := make([]byte, 64)
-	currentState := "off"
-
-	if path, err := findDevicePath(uint16(cfg.VendorID), uint16(cfg.ProductID)); err == nil && path != "" {
-		currentState = "on"
-		setDefaultAudioDevice(cfg.HeadsetName)
-	}
+	currentState := "unknown"
 
 	for {
 		n, err := dev.Read(buf)
